e4d433db61617f1941e85401ad244f3437c49068
Niks6455 <7929189niks64@gmail.com>
2023-08-25 17:30:44 +0300

    lodging

---
+++ fitchacode/src/component/button/button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './button.module.scss'
+
+const Button = ({text, bg_color, text_color, h, w,size }) => {
+	return (	
+                <button className={styles.buttons}  style={{ background: `${bg_color}`, fontSize: `${size +'px'}`, color: `${text_color}`, width: `${w +'px'}`, height: `${h +'px'}`} }>
+                    {text}
+                </button>
+	)
+}
+
+export default Button
