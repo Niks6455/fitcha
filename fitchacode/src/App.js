1e62054415eead28e6cd46df70444eb314806454
Artur <artur.tonoyan.04@list.ru>
2023-08-26 04:07:35 +0300

    end

--- fitchacode/src/App.js
+++ fitchacode/src/App.js
@@ -3,9 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import AppContext from "./context";
 import Interests from "./pages/Interests";
 import SelectionTour from "./pages/SelectionTour";
-import HomePage from "./pages/homepage/homePage";
 import Register from "./pages/register/Register";
-import Authorization from "./pages/autorization/Authorization";
 import Lodging from "./pages/lodging/lodging";
 import Active from "./pages/active/active";
 import img1 from "./images/1.svg";
@@ -17,7 +15,6 @@ import cafe3 from "./images/demon.svg";
 import Eade from "./pages/eade/eade";
 import Authorization from "./pages/autorization/Authorization.jsx";
 import Constructor from "./pages/Constructor";
-import MapConstructor from "./pages/MapConstructor";
 
 function App() {
   const points = [
@@ -29,7 +26,7 @@ function App() {
     <AppContext.Provider value={{ img1, img2, img3, cafe1, cafe2, cafe3 }}>
       <div className="App">
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Authorization />} />
           <Route path="Register" element={<Register />} />
           <Route path="Interests" element={<Interests />} />
           <Route path="SelectionTour" element={<SelectionTour />} />
@@ -37,10 +34,6 @@ function App() {
           <Route path="Active" element={<Active />} />
           <Route path="Eade" element={<Eade />} />
           <Route path="Constructor" element={<Constructor />} />
-          <Route
-            path="MapConstructor"
-            element={<MapConstructor points={points} />}
-          />
         </Routes>
       </div>
     </AppContext.Provider>
